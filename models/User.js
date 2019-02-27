61337b1f1a222d8058182ff1d118a37e5aa24f0e
jobi <olajobinurudeen2013@gmail.com>
2019-02-27 23:03:28 +0100

    create a model and  miigration for user

---
+++ models/User.js
@@ -0,0 +1,31 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+    var User = sequelize.define('User', {
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDv4,
+
+            allowNull: false,
+            primaryKey:true
+        },
+        username: {
+            type:DataTypes.STRING,
+            allowNull: true
+        },
+        firstname: {
+            type:DataTypes.STRING,
+            allowNull: true
+        },
+        password: {
+            type:DataTypes.STRING,
+            allowNull: true
+        },
+        email: {
+            type:DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+
+    })
+    return User;
+}
