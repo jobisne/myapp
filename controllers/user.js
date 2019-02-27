a2dce75e0fed05b9c295ba5a844207f3597538bb
jobi <olajobinurudeen2013@gmail.com>
2019-02-27 23:01:50 +0100

    Implement route for login and sign up

---
+++ controllers/user.js
@@ -0,0 +1,10 @@
+const models = require('../models');
+
+
+exports.show_login = function(req, res, next) {
+    res.render('user/login', { formData: {}, errors: {} });
+}
+
+exports.show_signup = function(req, res, next) {
+    res.render('user/signup', { formData: {}, errors: {} });
+}
